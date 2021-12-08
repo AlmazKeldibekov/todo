132e39f99d1bc5a6f53f239ef18df0769070fd7b
Almaz <a.keldibekov.k@gmail.com>
2021-12-08 19:46:14 +0600

    add detail view and put function

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework import  status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from tasks.models import Task
 from tasks.seralizers import TaskSerializer
 
 
 class TaskAPIView(APIView):
 
-    def get(self,request):
+    def get(self, request):
         tasks = Task.objects.all()
 
         serializer = TaskSerializer(tasks, many=True)
@@ -27,16 +28,22 @@ class TaskAPIView(APIView):
         return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class TaskDetailAPIView(APIView):
 
-    # def put(self,request,id):
-    #     task = get_object_or_404(Task, id=id)
-    #     serializer = TaskSerializer(instance=task, data=request.data)
-    #
-    #     if serializer.is_valid():
-    #         body = serializer.validated_data.gte('body')
-    #         est = serializer.validates_data.get('estimated_finish_time')
-    #         task.body = body
-    #         task.estimated_finish_time = est
-    #         task.save()
-    #         return Response(serializer.data)
-    #     return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task)
+        return Response(serializer.data)
+
+    def put(self,request,id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task, data=request.data)
+
+        if serializer.is_valid():
+            body = serializer.validated_data.get('body')
+            est = serializer.validated_data.get('estimated_finish_time')
+            task.body = body
+            task.estimated_finish_time = est
+            task.save()
+            return Response(serializer.data)
+        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
